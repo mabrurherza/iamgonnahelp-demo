eb66fa42a090e053a9044c6c8cb15e7eafb1d422
Mabrur Herzaridano <mabrur@Mabrurs-MacBook-Air.local>
2022-11-20 10:55:35 +0700

    I am gonna help landing page demo

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        baloo: "'Baloo 2', 'sans-serif'",
+        inter: "'Inter', sans-serif",
+      },
+    },
+  },
+  plugins: [],
+};
